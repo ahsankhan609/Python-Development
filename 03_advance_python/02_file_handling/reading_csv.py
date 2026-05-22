2fef1178f3b53472dfffbb1df47526e1a1369f70
ahsankhan609 <ahsankhan609@gmail.com>
2026-05-22 12:08:50 +0500

    refactor: replace namedtuple with NamedTuple for employee data handling

--- 03_advance_python/02_file_handling/reading_csv.py
+++ 03_advance_python/02_file_handling/reading_csv.py
@@ -1,16 +1,36 @@
+"""Read employees.csv and print each row as an Employee namedtuple."""
+
 import csv
-from collections import namedtuple
+from pathlib import Path
+from typing import NamedTuple
 
-with open("employees.csv", mode="r", encoding="utf-8") as csv_file:
-    reader: _reader = csv.reader(csv_file)
-    Employee: type[Employee] = namedtuple(
-        "Employee", next(reader), rename=True)
-    for row in reader:
-        employee = Employee(*row)
-        print(employee)
 
-# Employee(name='Linda', job='Technical Lead', email='linda@example.com')
-# Employee(name='Joe', job='Senior Web Developer', email='joe@example.com')
-# Employee(name='Lara', job='Project Manager', email='lara@example.com')
-# Employee(name='David', job='Data Analyst', email='david@example.com')
-# Employee(name='Jane', job='Senior Python Developer', email='jane@example.com')
+def read_csv_file(csv_path: Path) -> None:
+    """_Return data inside the CSV File_.
+
+    Args:
+        csv_path (Path): CSV file name with path
+
+    """
+    try:
+        # csv_path = Path(__file__).parent / "data/employees.csv"
+        with csv_path.open(mode="r", encoding="utf-8") as csv_file:
+            reader = csv.reader(csv_file)
+            Employee = NamedTuple(
+                "Employee", [(field, str) for field in next(reader)])
+            for row in reader:
+                employee = Employee(*row)
+                print(employee)
+                """
+                print statements used for debugging should be omitted from production code. They can lead the accidental inclusion of sensitive information in logs, and are not configurable by
+                clients, unlike logging statements.
+                https://docs.astral.sh/ruff/rules/print/
+                """  # noqa: E501
+    except FileNotFoundError:
+        print("The file 'employees.csv' was not found.")
+    except OSError as e:
+        print(f"An error occurred while reading the file: {e}")
+
+
+if __name__ == "__main__":
+    read_csv_file(Path(__file__).parent / "data/employees.csv")
