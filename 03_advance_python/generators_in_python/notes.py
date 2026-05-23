cfe6827b7d580edb183bd41a0e5aec0aa5c5aa9a
ahsankhan609 <ahsankhan609@gmail.com>
2026-05-23 12:47:20 +0500

    chore: add video resources for generators in Python to notes

---
+++ 03_advance_python/generators_in_python/notes.py
@@ -0,0 +1,4 @@
+https://www.youtube.com/watch?v=bwEM8BAlq14
+https://youtu.be/1OSEzdOpmWQ?si=D5-dRo-R7kjKxrvQ
+https://www.youtube.com/watch?v=G1lJeEIl05o
+https://www.youtube.com/watch?v=tmeKsb2Fras
