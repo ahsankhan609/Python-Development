31dd17be2a4a289bc6be414ea1dec5715f4becc0
Ahsan Khan <ahsankhan609@gmail.com>
2026-05-28 13:24:37 +0500

    update notes

--- 02_intermediate_python/03_error_and_exceptions_handling/error_handling_codeyug/src/basic_exceptions.py
+++ 02_intermediate_python/03_error_and_exceptions_handling/error_handling_codeyug/src/basic_exceptions.py
@@ -1,6 +1,7 @@
 """
 An exception is an event which occurs during the execution of program that disrupts the normal flow of program. It is a situation that python can't cope with it.
 """
+
 """
 Why it is dangerous?
 
@@ -34,29 +35,39 @@ except:
     # code that will be executed if an exception is raised, due to error in try block
     print("An exception occurred. from except block.")
 else:
-    # code that will be executed if no exception is raised and the try block is executed successfully
+    # code that will be executed if no exception is raised and the try block is executed
+    # successfully
     print("No exception occurred. from else block.")
 finally:
-    # code that will be executed(always) regardless of whether an exception is raised or not. for example: closing a file, closing a database connection, etc.
+    # code that will be executed(always) regardless of whether an exception is raised or not
+    # for example: closing a file, closing a database connection, etc.
     print("Finally block executed. from finally block.")
 
 # now let's see when there is an error in try block:
 try:
     # code that may raise an exception
-    division_by_zero: float = 10/0
+    division_by_zero: float = 10 / 0
     print(f"Division result: {division_by_zero}")
-    # we are raising an exception manually, so it will raise an exception and go to except block
+    # we are raising an exception manually, so it will raise an exception
+    # and go to except block
     raise Exception("This is a test exception.")
 except Exception as e:
     # code that will be executed if an exception is raised, due to error in try block
     print(
-        f"An exception occurred. from except block. {type(e).__name__}: {e}, is not allowed.")
+        f"An exception occurred. from except block. {type(e).__name__}: {e}, is not allowed."
+    )
 else:
-    # code that will be executed if no exception is raised and the try block is executed successfully
-    print("No exception occurred. from else block. No error in try block. So this block will not be executed.")
+    # code that will be executed if no exception is raised
+    # and the try block is executed successfully
+    print(
+        "No exception occurred. from else block. No error in try block. So this block will not be executed."
+    )
 finally:
-    # code that will be executed(always) regardless of whether an exception is raised or not. for example: closing a file, closing a database connection, etc.
-    print("Finally block executed. from finally block. Even if there is an exception, this block will be executed.")
+    # code that will be executed(always) regardless of whether an exception is raised or not.
+    # for example: closing a file, closing a database connection, etc.
+    print(
+        "Finally block executed. from finally block. Even if there is an exception, this block will be executed."
+    )
 
     """
     all built-in exceptions are subclasses of Exception class.
--- 02_intermediate_python/03_error_and_exceptions_handling/error_handling_codeyug/src/custom_exception.py
+++ 02_intermediate_python/03_error_and_exceptions_handling/error_handling_codeyug/src/custom_exception.py
@@ -1,16 +1,20 @@
 """
-Python has many built-in exceptions, such as ValueError, TypeError, IndexError, and about 64 others, but they are all very general. Many times, it can be much better to make tailored exceptions for specific scenarios in your projects.
+Python has many built-in exceptions, such as ValueError, TypeError, IndexError.
+and about 64 others, but they are all very general. Many times, it can be much better
+to make tailored exceptions for specific scenarios in your projects.
 
 https://jacobpadilla.com/writing/Custom-Python-Exceptions
 """
 
 
 class CustomException(Exception):
-    """This is a Custom Exception class to raise custom exceptions for specific scenarios in your projects.
+    """This is a Custom Exception class to raise custom exceptions for
+     specific scenarios in your projects.
 
     Args:
         Exception (Exception): Exception class
     """
+
     pass
 
 
