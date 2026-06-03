ca344715368c3e455e71dbb152609b975e838a1f
Ahsan Khan <ahsankhan609@gmail.com>
2026-06-03 12:00:47 +0500

    Refactor variable naming and clean up code in refactoring_var_name.py

--- 01_basic_python/refactoring_var_name.py
+++ 01_basic_python/refactoring_var_name.py
@@ -1,17 +1,14 @@
 """
 sometimes we write a program with bunch of keywords or function names. but after
-sometimes we need to change them. so the best approach is to choose the function name
-or variable name and right click on it, choose Refactor->Rename it, etc.
+sometimes we need to change them. so the best approach is to choose the function name or variable name and right click on it, choose Refactor->Rename it, etc.
+
+this method is not available in all IDEs. but it is available in PyCharm.
 """
 
 
 def main() -> None:
     """this is just placeholder."""
-
-    def foo():
-        """this is a test function"""
-
-        pass
+    pass
 
 
 if __name__ == "__main__":
