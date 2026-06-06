d6ef41e11710f2466032a83e75e4a0271f80e825
Ahsan Khan <ahsankhan609@gmail.com>
2026-06-06 14:23:44 +0500

    Remove obsolete generators placeholder at old path
    Deletes 03_advance_python/01_generators_in_python/notes.py after folding
    generators content into the iterators_generators topic folder.

--- 03_advance_python/01_generators_in_python/notes.py
+++
@@ -1,4 +0,0 @@
-https://www.youtube.com/watch?v=bwEM8BAlq14
-https://youtu.be/1OSEzdOpmWQ?si=D5-dRo-R7kjKxrvQ
-https://www.youtube.com/watch?v=G1lJeEIl05o
-https://www.youtube.com/watch?v=tmeKsb2Fras
