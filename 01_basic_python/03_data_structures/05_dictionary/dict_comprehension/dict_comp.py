23498a6c317686d58834dbc7b36de3dcedd9c711
Ahsan Khan <ahsankhan609@gmail.com>
2026-06-06 14:20:20 +0500

    Add 01_basic_python/03_data_structures/05_dictionary/dict_comprehension/dict_comp.py to renumbered data structures folder
    Places data-structure lessons under 03_data_structures with numbered
    subfolders and a 00_overview section for comparison notes.

---
+++ 01_basic_python/03_data_structures/05_dictionary/dict_comprehension/dict_comp.py
@@ -0,0 +1,5 @@
+# https://www.youtube.com/watch?v=cQjcQPF6CNA
+# https://www.youtube.com/watch?v=5haRWWIKTro
+# https://www.youtube.com/watch?v=wxd-rQkmq4Y
+# https://www.youtube.com/watch?v=f2LWNd_fkJE
+# https://www.youtube.com/watch?v=khCzymLy_QE
