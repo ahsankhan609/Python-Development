b722d31c62e174225854fe6fdb24fcbbe213d3b9
Ahsan Khan <ahsankhan609@gmail.com>
2026-06-06 14:24:33 +0500

    Add 04_misc_python/scratch/test.py to dedicated misc module subfolder
    Moves a loose 04_misc_python script into a topic-specific subfolder created
    during tidy-project (linting, scratch, or docstrings).

---
+++ 04_misc_python/scratch/test.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+n: int = 100000000  # if we use _ interpreter will ignore it
+print(n)
+print(f'{n:_}')
+print(f'{n:,}')
+
+
+def main() -> None:
+    now: datetime = datetime.now()
+    print(f'{now:%d. %m %y (%H:%M:%S)}')
+    print(f'{now:%c}')  # return local datetime
+    print(f'{now:%I%p}')  # return 24HRS time
+
+
+if __name__ == '__main__':
+    main()
