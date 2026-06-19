4a45434d598467f32975d469b057a6c1c520428f
Ahsan Khan <ahsankhan609@gmail.com>
2026-06-19 12:15:10 +0500

    Add documentation comments to reading_env_variables.py for environment variable handling and custom .env file usage.

--- 04_misc_python/secrets_module/reading_env_variables.py
+++ 04_misc_python/secrets_module/reading_env_variables.py
@@ -1,3 +1,9 @@
+"""Reading environment variables from a .env file.
+
+KEY POINTS:
+* It is a way to store sensitive information in a file.
+"""
+
 import os
 from pathlib import Path
 
@@ -6,11 +12,20 @@ from dotenv import load_dotenv
 # https://youtu.be/8dlQ_nDE7dQ
 # https://youtu.be/PYuTzLswn_Y
 # https://youtu.be/A1OA0Y9vwJY
+# https://youtu.be/riptPbTMLVQ
 
 # parents[0] = reading_env_variables/, parents[1] = 04_misc_python/, parents[2] = project root
 _ENV_FILE: Path = Path(__file__).parents[2] / ".env"
 
 load_dotenv(_ENV_FILE)  # injects .env variables into os.environ
+
+# load_dotenv(dotenv_path="custom_env_file.env")
+# if we have custom name of our env file, then load it here to use it,
+# instead of the default .env file.
+# but why we use custom environment variables file?
+# because we can have multiple env files for different environments,
+# like development(.env.development), production(.env.production), testing(.env.testing), etc.
+# and we can load the appropriate env file for the environment we are in.
 
 print(os.getenv("SWAPI_BASE_URL"))
 print(os.getenv("COMBINED"))
